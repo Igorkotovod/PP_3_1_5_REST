eb3ca3b25ef2ff5d772badbc613a713dda39d654
Igor Pavlov <my-mail.msk@mail.ru>
2023-05-28 12:44:47 +0300

    2.4

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,19 +3,22 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/admin")
@@ -23,10 +26,12 @@ import java.util.List;
 public class AdminController {
 
     private final UserServiceImpl userService;
+    private final RoleServiceImpl roleService;
 
     @Autowired
-    public AdminController(UserServiceImpl userService) {
+    public AdminController(UserServiceImpl userService, RoleServiceImpl roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping("users")
@@ -42,8 +47,14 @@ public class AdminController {
 
     @PostMapping(value = "users")
     public ResponseEntity<User> newUser(@RequestBody User user) {
-            userService.saveUser(user);
+        userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<User> update(@RequestBody User user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("users/{id}")
@@ -52,6 +63,20 @@ public class AdminController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @GetMapping("/user")
+    public ResponseEntity<User> showUser(Authentication auth) {
+        return new ResponseEntity<>(userService.findUserByUsername(auth.getName()), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return new ResponseEntity<>(roleService.listAllRoles(), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles/{id}")
+    ResponseEntity<Role> getRoleById(@PathVariable("id") long id){
+        return new ResponseEntity<>(roleService.findRoleById(id), HttpStatus.OK);
+    }
 /*
     @GetMapping
     public String listUser(Model model, Principal principal) {
