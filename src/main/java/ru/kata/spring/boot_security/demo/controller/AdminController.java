48b54664a3e433cf8bd61287f4f51f3f477dd5ab
Igor Pavlov <my-mail.msk@mail.ru>
2023-05-30 22:05:55 +0300

    REST_controller_ALL_OK

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,83 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+import java.security.Principal;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserServiceImpl userService;
+    private final RoleServiceImpl roleService;
+
+    @Autowired
+    public AdminController(UserServiceImpl userService, RoleServiceImpl roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String listUser(Model model, Principal principal) {
+        model.addAttribute("newUser", new User());
+        model.addAttribute("user", userService.findUserByUsername(principal.getName()));
+        model.addAttribute("listRoles", roleService.listAllRoles());
+        model.addAttribute("listUser", userService.listAllUsers());
+        return "admin";
+    }
+
+    @GetMapping("/{id}")
+    public String showUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "redirect:/user";
+    }
+
+    @GetMapping("/new")
+    public String addNewUser(Model model) {
+        List<Role> roles = roleService.listAllRoles();
+        model.addAttribute("roles", roles);
+        model.addAttribute("user", new User());
+        return "redirect:/";
+    }
+
+    @PostMapping()
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/editUser")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.getUser(id));
+        List<Role> roles = roleService.listAllRoles();
+        model.addAttribute("rolesAdd", roles);
+        return "redirect:/admin";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,24 +1,24 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 import java.security.Principal;
 
-@RestController
-@RequestMapping("api/user")
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
